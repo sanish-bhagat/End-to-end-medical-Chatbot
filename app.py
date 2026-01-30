589dffa789f9aed30facade4cf9b7d90fee61fb7
Sanish Bhagat <sanishbhagat3@gmail.com>
2026-01-30 10:13:26 +0530

    module fix

--- app.py
+++ app.py
@@ -3,8 +3,8 @@ from src.helper import download_hugging_face_embeddings
 from langchain_pinecone import Pinecone
 from langchain_google_genai import ChatGoogleGenerativeAI
 # from langchain import create_retrieval_chain
-from langchain.chains.retrieval import create_retrieval_chain
-from langchain.chains.combine_documents import create_stuff_documents_chain
+from langchain_classic.chains.retrieval import create_retrieval_chain
+from langchain_classic.chains.combine_documents import create_stuff_documents_chain
 from langchain_core.prompts import ChatPromptTemplate
 from dotenv import load_dotenv
 from src.prompt import *
